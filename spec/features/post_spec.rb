320b225bc08f3b67029d13c4a0734034876f3794
Matt Farmer <mrfarmer777@gmail.com>
2018-07-09 04:10:24 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,12 +5,21 @@ describe 'new post' do
     visit new_post_path
     expect(page.status).to eq(200)
   end
-
+  
 
 
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content("Post Form")
   end
+
+  it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
+    visit new_post_path
+    fill 'post_title', with: "My post title"
+    fill_in 'post_description', with: 'My post description'
+
+    click_on 'Submit Post'
+
+    expect(page.current_path).to eq(posts_path)
 
 end
