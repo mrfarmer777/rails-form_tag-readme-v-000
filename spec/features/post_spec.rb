32d3ca72434cc65dbe88c4393f969196bde5586f
Matt Farmer <mrfarmer777@gmail.com>
2018-07-09 04:07:32 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
+    expect(page.status).to eq(200)
+  end
+  
+
+
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content("Post Form")
+  end
 
 end
